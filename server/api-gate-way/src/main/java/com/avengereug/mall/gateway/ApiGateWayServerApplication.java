2fdbd6c6b550866e11dda0114cbfbbb6e644846a
AvengerEug <eugenesumarry@163.com>
2020-07-21 21:05:22 +0800

    feat(5): 新增api gate way网关，并根据官网案例做集成测试

---
+++ server/api-gate-way/src/main/java/com/avengereug/mall/gateway/ApiGateWayServerApplication.java
@@ -0,0 +1,12 @@
+package com.avengereug.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateWayServerApplication.class, args);
+    }
+}
