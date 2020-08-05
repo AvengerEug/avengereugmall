3c8fcd0e4043b20da9b92b0a2c3eae0ffefe42c5
AvengerEug <eugenesumarry@163.com>
2020-08-05 22:31:17 +0800

    feat(30): 新增service-es 操作elasticsearch模块

---
+++ service/service-es/src/main/java/com/avengereug/mall/es/ServiceESApplication.java
@@ -0,0 +1,12 @@
+package com.avengereug.mall.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceESApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceESApplication.class, args);
+    }
+}
---
+++ service/service-es/src/main/java/com/avengereug/mall/es/config/ElasticsearchConfig.java
@@ -0,0 +1,20 @@
+package com.avengereug.mall.es.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+@Configuration
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("192.168.213.128:9200")
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
