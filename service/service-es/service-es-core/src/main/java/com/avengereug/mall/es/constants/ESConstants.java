8e031a9892dcd9b221a614c76111833378faaf71
AvengerEug <eugenesumarry@163.com>
2020-09-15 00:26:13 +0800

    feat(42): 商品搜索页集成后端es完成检索功能

--- service/service-es/service-es-core/src/main/java/com/avengereug/mall/es/constants/ESConstants.java
+++ service/service-es/service-es-core/src/main/java/com/avengereug/mall/es/constants/ESConstants.java
@@ -2,7 +2,7 @@ package com.avengereug.mall.es.constants;
 
 public final class ESConstants {
 
-    public static final String INDEX_PRODUCT = "product";
+    public static final String INDEX_PRODUCT = "product2";
 
     public static final Integer PRODUCT_PAGESIZE = 16;
 
